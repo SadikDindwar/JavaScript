ba323ded2e74f503137044cecc8e26bbe3159142
Sadik B <sadikdindwar@gmail.com>
2025-10-27 00:09:28 +0530

    updated

--- Learning/L1.js
+++ Learning/L1.js
@@ -1,6 +1,35 @@
-console.log("Hi");
-function ok(num){
-    return num * num;
-}
-const temp = ok(6);
-console.log(temp);
+//1. Data Types
+
+/*
+In JavaScript we have 8 types of data that is 7 primitive data types and 1 non-primitive data type object. 
+Primitive data types
+1.String
+2.Number
+3.Boolean
+4.BigInt
+5.Symbol
+6.Undefined
+7.Null
+
+Non-Primitive data type 
+8.Object
+*/
+
+//String
+//String can be declared in ' ' or in " "
+let d1 = "Hello";
+let d2 = 'hi';
+
+console.log(typeof d1); // typeof is used to know the data type
+console.log(typeof d2);
+
+//Undefined 
+//Undefined is the type of primitive data type which means thata variable exists but has not been assigned any value yet.
+let d3;
+
+console.log(typeof d3); // op : undefind
+
+console.log(typeof 79)
+
+
+
