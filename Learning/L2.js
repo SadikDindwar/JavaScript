c179f002ed297f4330b09a0efea74eaa64b82ebc
Sadik B <sadikdindwar@gmail.com>
2025-10-27 01:31:27 +0530

    changed

--- Learning/L2.js
+++ Learning/L2.js
@@ -5,3 +5,12 @@
 console.log(typeof 90);
 console.log(typeof 9.9);
 
+//If number is added to a String then they will be concatinated and returned as String
+
+let v1 = "12";
+let v2 = 5;
+
+let v3 = v1+v2;
+console.log(v3);
+console.log(typeof v3)
+
