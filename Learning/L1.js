ff074b4c64566951c5bc8a1671a7b136bf3bdd17
Sadik B <sadikdindwar@gmail.com>
2025-10-27 01:23:51 +0530

    changed

--- Learning/L1.js
+++ Learning/L1.js
@@ -42,7 +42,7 @@ console.log(v1.charAt(2))
 //indexOf() used to check index of particular character in a given string
 console.log(v1.indexOf('s'))
 console.log(v1.indexOf('z')) // if character is not present in a given string then it will return -1
-
+console.log(v1.indexOf("ip"))
 
 //length this is the property which return no of character in a given String
 console.log(v1.length)
@@ -55,3 +55,19 @@ console.log(v1.lastIndexOf("ipt")) // 7 coz ipt starts from 7th index
 console.log(v1.slice(0, 1)) // here ending index is exclusive or endind index-1
 console.log(v1.slice(1, 3))
 console.log(v1.slice(11)) // if we donot provide the ending index it will return all the character from starting endex
+
+
+//includes() this method is used to check if the character or the string is present in a given string.
+console.log(v1.includes("i"))
+console.log(v1.includes("pro"))
+console.log(v1.includes("z"))
+console.log(v1.includes("prod"))
+
+
+//split() this method will split the given string from the character we provide and return an array of string
+console.log(v1.split(" "))
+console.log(v1.split("a"))
+
+
+
+
