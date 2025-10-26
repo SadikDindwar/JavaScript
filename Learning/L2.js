a4f7d181539838bb57b44f08a6fddc76f6c9bde4
Sadik B <sadikdindwar@gmail.com>
2025-10-27 01:40:15 +0530

    changed

--- Learning/L2.js
+++ Learning/L2.js
@@ -14,3 +14,29 @@ let v3 = v1+v2;
 console.log(v3);
 console.log(typeof v3)
 
+// We can change the String type to number by using Number() function
+
+let v4 = Number(v3);
+console.log(typeof v4)
+
+
+//false=0 and true =1
+console.log(Number(true))
+console.log(Number(false)) 
+
+//Number Functions
+
+//Number.isInteger() funtion determines weather the passed value is the integer or not
+console.log(Number.isInteger(2))
+console.log(Number.isInteger(v1))
+console.log(Number.isInteger(v2))
+
+
+
+
+
+
+
+
+
+
