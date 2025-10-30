d1e2bf17a9aa9b328e88cb36cb4b65981d6d263a
Sadik B <sadikdindwar@gmail.com>
2025-10-30 23:52:38 +0530

    changed

--- Learning/L4.js
+++ Learning/L4.js
@@ -1,2 +1,22 @@
 // User inputs 
 
+// confirm("Ok for confirm \n Cancel for canceling."); // confirm() will generate the pop up saying ok or cancel.
+
+
+// '??'
+// ?? in JavaScript is the nullish coalescing operator.
+//Syntax : a ?? b  -> this means if return a is not null or undefined otherwise return b.
+//example
+let a; // here a is undefined.
+let b =10;
+console.log(a??b);  
+
+let c;
+console.log(c??"c is empty"); // we can also return directly
+
+let d =79;
+let e =56;
+console.log(d ?? e);
+
+
+
