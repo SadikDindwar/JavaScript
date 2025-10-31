36bbf374b03c593aea13e7bfa8316507d9461860
Sadik B <sadikdindwar@gmail.com>
2025-10-31 15:48:05 +0530

    updated

--- Learning/L4.js
+++ Learning/L4.js
@@ -19,4 +19,73 @@ let e =56;
 console.log(d ?? e);
 
 
+// Functions
+
+//Regular Functions
+
+function sum(num1 , num2){
+    return num1+num2;
+}
+
+let v1 = function multiply(num1 , num2){
+    return num1*num2;
+};
+
+let v2 = v1(12 , 5);
+console.log(v2);
+
+function doSomething(num1 , num2=2){// here if we donot provide second argument then by defaul 2 will 
+    return num1+num2;
+}
+console.log(doSomething(12, 5));
+console.log(doSomething(10));  // second argument by defaul 2 will be set
+
+
+//Anonymus Function : Anonymus function is the type of function which donot have name ,and it can be passed as a call back to another functions.
+//Example
+
+let v3 = function(num1){
+    return num1+10;
+
+}
+console.log(v3(10));
+
+setTimeout(function(){
+    console.log("Statement executed after 5 sec");
+
+}, 5000)
+
+
+// Arrow Functions : Arrow functions are type of functions which donot have name and a fumction keyword
+//introduced in ES6. it reduces the syntax.
+//Example
+
+let v4 = ()=> {
+    console.log("Arrow func");
+    return 2;
+}
+console.log(v4());
+ 
+//or
+
+let v5 = (num1)=> {
+    return num1;
+}
+console.log(v5(5));
+
+//or
+
+let v6 = num2 => num2+num2; // here if a arrow func only have one param the we can skip the brackets
+//and if it has only one statement we can skip curly brackets and return keyword as shown above.
+console.log(v6(3));
+
+let v7 = (num1, num2) => num1+num2;
+console.log(v7(1,5));
+
+
+
+
+
+
+
 
--- Practice/P3.js
+++ Practice/P3.js
@@ -6,3 +6,19 @@ while(num<=50){
     console.log(num);
     num++;
 }
+
+//write a Js func which extracts the name from email
+
+function extractName(email){
+    return email.slice(0 , email.indexOf("@"));
+}
+
+console.log(extractName("alice@gmail.com"));
+
+
+//write a Js function which make name in correct format like first letter Capital and remainiing letter small.
+
+function nameFormat(name){
+    return name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+}
+console.log(nameFormat("aLiCe"));
