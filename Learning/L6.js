17a5303d7f8755b09cafe12cf92f604f14925217
Sadik B <sadikdindwar@gmail.com>
2025-10-31 19:51:42 +0530

    updated

---
+++ Learning/L6.js
@@ -0,0 +1,24 @@
+// Arrays.
+
+const myArray = [];
+myArray[0]=10;
+myArray[1]="sadik";
+myArray[2]= true;
+
+console.log(myArray);
+
+myArray.push("added");
+console.log(myArray);
+
+
+// delete keyword
+// The delete keyword in JavaScript is used to remove properties from objects, not variables or functions.
+//example
+
+delete myArray[0];
+
+console.log(myArray);
+
+
+let v1 =90;
+// delete v1; // not allowed for vaiables 
