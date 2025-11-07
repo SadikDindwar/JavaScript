e87014a8d8f64024769ad6ab2594ea1b0603334d
Sadik B <sadikdindwar@gmail.com>
2025-11-08 00:57:38 +0530

    done

--- Learning/L8.js
+++ Learning/L8.js
@@ -1,1 +1,10 @@
-// High Order Functions (HOC) and Callbacks.
+// High Order Functions (HOF) and Callbacks.
+/*
+ High order functions are the type of functions which take callback functions as parametres or the functions which return another function.
+ example: map(),reduce(), setTimeout(callback, time_in_miliseconds)
+ */
+//Example for high order functions
+//setTimeout
+setTimeout(()=>{
+    console.log("hi");
+}, 5000)
