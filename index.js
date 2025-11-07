22c8830d24a923e861d47059d7d006e66bb4dd3d
Sadik B <sadikdindwar@gmail.com>
2025-11-08 04:02:21 +0530

    changed

--- index.js
+++ index.js
@@ -14,3 +14,19 @@ setTimeout(()=>{
 
 let v2 = f1(f2 , 4);
 console.log(v2);
+let v3 =0;
+
+
+
+
+document.getElementById("button1").addEventListener('click' ,()=>{
+    if(v3 < 10){
+        console.log("button clicked "+(++v3)+" times.");
+    }
+    else{
+        v3++;
+        console.log("button cannot be clicked more than 10 times.");
+        document.querySelector('body').style.backgroundColor = 'red';
+    }
+});
+
