21e8e230c202cb438f96d76268e6a2c0c8ea469b
Sadik B <sadikdindwar@gmail.com>
2025-11-12 03:54:47 +0530

    updated

--- Learning/L11.js
+++ Learning/L11.js
@@ -1,3 +1,15 @@
 // Async / Await (V.Imp)
 
+/*
+Async/Await: is JavaScript syntax that lets us write asynchronous code in a clean, step-by-step and readable way.
+It works on top of Promises, and behind the scenes it uses microtasks and the event loop to handle all the waiting.
 
+Async: Async is a keyword that is used before the function to tell the function will return a Promise and that fnction may have Asynchronous code.
+
+Await: Await is the keyword that is used inside the async function to pause the function execution until the promise gets resolved or rejected.
+
+
+
+
+
+*/
